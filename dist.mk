a5b4ec111c76730f36725aa11a50b3dd58d6f182
emonet <emonet@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2006-05-04 13:54:00 +0000

    ...

--- dist.mk
+++ dist.mk
@@ -1,7 +1,17 @@
 
+out=out
+
+release: all-html
+	rm -rf ${out}
+	mkdir ${out}
+	cp -r css image download *.html ${out}
+	cd ${out}          && find -name .svn -exec rm -rf {} \;
+	cd ${out}/download && wget http://oberon/release/omiscid.jar
+	cd ${out}/download && wget http://oberon/release/omiscidGui.jar
 
 all-html: $(patsubst %.xml,%.html,$(wildcard *.xml))
 
 %.html: %.xml $(wildcard xsl/*.xsl) $(wildcard common/*.xml)
 	xsltproc --xinclude --output $@ xsl/page.xsl $<
 	sed -i -e "s@[.]xml@.html@g" $@
+
